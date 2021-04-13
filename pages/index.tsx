b42965135b1b37bf099329cf6399c0f3c39875fc
Anuruddh <anuruddh@Aditis-Air.localdomain>
2021-04-13 11:47:34 +0530

    Added data management

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { Header, Main, Footer, Cards } from "@components";
+import {getUser} from "@data/user";
 
 const Home: React.FC = () => {
+const {data, error} = getUser()
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
       <Main />
-      <Cards />
+    {data ?
+    <p>The user's name is {data.name}</p>
+    :
+    <p>fetching user details...</p>}
       <Footer />
     </div>
   );
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -11,14 +11,6 @@ import {
 export const Footer: React.FC = () => {
   return (
     <div className="text-center py-4" style={{ backgroundColor: "#282c34" }}>
-      <a
-        href="https://github.com/pankod"
-        target="_blank"
-        className="d-block mb-3"
-      >
-        <PankodIcon color="white" width="140" height="28" />
-      </a>
-
       <ul className="d-flex justify-content-center list-unstyled p-0 m-0">
         <li className="mx-2">
           <GithubIcon color="white" width="28" height="29" />
